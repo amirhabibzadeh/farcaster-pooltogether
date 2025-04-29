df9b077b3544b6f78ac035c2122666b752788af5
Amir Habibzadeh <habibzade.amir@gmail.com>
2025-04-29 15:48:45 +0300

    fix: fix og image

--- src/constants/frame.ts
+++ src/constants/frame.ts
@@ -1,13 +1,13 @@
 export const frameConfig = {
     version: "next",
-    imageUrl: `${process.env.NEXT_PUBLIC_APP_URL}/api/frame/og-image`,
+    imageUrl: `${process.env.NEXT_PUBLIC_APP_URL}/pt-og-image.png`,
     button: {
         title: "PoolTogether Farcaster Mini App",
         action: {
             type: "launch_frame",
             url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
             name: "PoolTogether",
-            splashImageUrl: `${process.env.NEXT_PUBLIC_APP_URL}/logo.png`,
+            splashImageUrl: `${process.env.NEXT_PUBLIC_APP_URL}/pooltogetherLogo.svg`,
             splashBackgroundColor: "#1a1a1a"
         }
     }
